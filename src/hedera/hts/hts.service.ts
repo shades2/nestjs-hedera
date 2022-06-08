a1ab9d34499a0c8ddeb2649b90df4269bad1d531
HSuite Network <info@hbarsuite.network>
2022-06-08 17:53:27 +0200

    fixed bug on random nodes selection

--- src/hedera/hts/hts.service.ts
+++ src/hedera/hts/hts.service.ts
@@ -56,9 +56,10 @@ export class HtsService {
     return new Promise(async (resolve, reject) => {
       try {
         const client = this.clientService.getClient();
-        // generating random number from 3 to 28, as a workound for offline signature...
-        let nodeAccountId = Math.floor(Math.random() * (28 - 3 + 1) + 3);
-
+        // generating random number from 3 to 9, as a workound for offline signature...
+        let nodeAccountId = Math.floor(Math.random() * (9 - 3 + 1) + 3);
+        
+        console.log("using account ID", (new AccountId(nodeAccountId)).toString());
         const transaction = await new TokenAssociateTransaction()
           // setting single node accountId, as a workound for offline signature...
           .setNodeAccountIds([new AccountId(nodeAccountId)])
