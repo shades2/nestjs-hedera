a7eec3a84a6299d1ce9c97284c7510fa1d32bf63
HSuite Network <info@hbarsuite.network>
2022-07-31 19:01:47 +0200

    added rest API for HCS

---
+++ lib/hedera/hcs/hcs-rest.service.d.ts
@@ -0,0 +1,13 @@
+import { RestService } from '../rest/rest.service';
+/**
+ * Injectable
+ */
+export declare class HcsRestService {
+    private restService;
+    /**
+     * Transaction Rest Service
+     * @param {RestService} restService
+     */
+    constructor(restService: RestService);
+    getLatestMessages(topicId: string): Promise<any>;
+}
---
+++ src/hedera/hcs/hcs-rest.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { RestService } from '../rest/rest.service';
+
+/**
+ * Injectable
+ */
+@Injectable()
+export class HcsRestService {
+
+  /**
+   * Transaction Rest Service
+   * @param {RestService} restService 
+   */
+  constructor(
+    private restService: RestService
+  ) { }
+
+  getLatestMessages(topicId: string): Promise<any> {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let response = await this.restService
+          .call(`topics/${topicId}/message`);
+
+        resolve(response);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
+}
