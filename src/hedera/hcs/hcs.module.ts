f211d33ded4f65cbe5494b6504ae4ba8e816c4fd
HSuite Network <info@hbarsuite.network>
2022-07-31 18:58:15 +0200

    added rest API for HCS

--- src/hedera/hcs/hcs.module.ts
+++ src/hedera/hcs/hcs.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { HcsService } from './hcs.service';
 import { ClientModule } from '../client/client.module';
 import { RestModule } from '../rest/rest.module';
+import { HcsRestService } from './hcs-rest.service';
 
 @Module({
   imports: [ClientModule, RestModule],
   controllers: [],
-  providers: [HcsService],
-  exports: [HcsService]
+  providers: [HcsService, HcsRestService],
+  exports: [HcsService, HcsRestService]
 })
 export class HcsModule {}
