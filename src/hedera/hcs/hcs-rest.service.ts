628e33d8f2c3b8bb3fbb2f6fbcc29e5c1df9ff7a
HSuite Network <info@hbarsuite.network>
2022-09-01 12:03:18 +0200

    added pagination on HCS method

--- src/hedera/hcs/hcs-rest.service.ts
+++ src/hedera/hcs/hcs-rest.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { RestService } from '../rest/rest.service';
+import * as lodash from 'lodash';
 
 /**
  * Injectable
@@ -31,8 +32,32 @@ export class HcsRestService {
   getLatestMessagesFromTimestamp(topicId: string, timestamp: number): Promise<any> {
     return new Promise(async (resolve, reject) => {
       try {
+        let messages = [];
         let response = await this.restService
           .call(`topics/${topicId}/messages?order=desc&timestamp=gte:${timestamp}.000000000`);
+
+        messages = messages.concat(response.messages);
+        let retry = false;
+        let timeout = 0;
+
+        while (response.links.next || retry) {
+          if(timeout) {
+            await new Promise(resolve => setTimeout(resolve, timeout));
+          }
+
+          let next = lodash.get(response.links.next.split("?"), 1);
+
+          try {
+            response = await this.restService
+            .call(`topics/${topicId}/messages?${next}`);
+
+            messages = messages.concat(response.messages);
+            retry = false;
+          } catch(error) {
+            timeout += 100;
+            retry = true;
+          }
+        }          
 
         resolve(response);
       } catch (error) {
